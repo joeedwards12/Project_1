68d14eca04eb2ac3375e6d055f8cc7a4e5f3d2a7
joeedwards12 <130602395+joeedwards12@users.noreply.github.com>
2023-10-18 19:56:38 +0100

    Updated workflow for test

--- network.tf
+++ network.tf
@@ -59,25 +59,12 @@ resource "aws_route_table_association" "public_subnet_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnets[1].id
-}
-
-resource "aws_eip" "nat_eip" {}
-
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.Project_1_vpc.id
 
   tags = {
     Name = "Private Subnets Route Table"
   }
-}
-
-resource "aws_route" "nat_gateway_route" {
-  route_table_id         = aws_route_table.private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.nat_gateway.id
 }
 
 resource "aws_route_table_association" "private_subnet_association" {
